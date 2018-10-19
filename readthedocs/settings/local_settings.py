06b3a48b6106a177dd8b13ccc83110641b259c6d
immber <jessiekrushing@hotmail.com>
2018-10-18 22:03:26 -0700

    added local_settings.py to git

---
+++ readthedocs/settings/local_settings.py
@@ -0,0 +1,21 @@
+import os
+
+SECRET_KEY = 'no really its a secret' 
+
+PRODUCTION_DOMAIN = 'talk-documentation.herokuapp.com:8000'
+WEBSOCKET_HOST = 'talk-documentation.herokuapp.com:8088'
+
+import django_heroku
+django_heroku.settings(locals())
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
+
+
+import dj-database-url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES[‘default’].update(db_from_env)
