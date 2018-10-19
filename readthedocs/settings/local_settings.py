588e21a25f87d7867150300270fd634d0525a275
immber <jessiekrushing@hotmail.com>
2018-10-19 10:21:28 -0700

    adding engine

--- readthedocs/settings/local_settings.py
+++ readthedocs/settings/local_settings.py
@@ -14,7 +14,6 @@ django_heroku.settings(locals())
 import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)
 
-DATABASES['default'] = {
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-}
+
 DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
