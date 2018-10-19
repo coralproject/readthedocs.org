65e9d303af5e9d58767cc7fc0f1e8a270f77d4f8
immber <jessiekrushing@hotmail.com>
2018-10-18 22:21:00 -0700

    adding local settings for pg

--- readthedocs/settings/local_settings.py
+++ readthedocs/settings/local_settings.py
@@ -11,4 +11,4 @@ django_heroku.settings(locals())
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': db_from_env}
